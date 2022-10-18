c1b899c280268fb9ca143174bcfae44307895475
Batuhacar <114250712+Batuhacar@users.noreply.github.com>
2022-10-18 11:57:40 +0300

    Add files via upload

---
+++ abstractClasses/src/abstractClasses/MenGameCalculator.java
@@ -0,0 +1,8 @@
+package abstractClasses;
+
+public class MenGameCalculator extends GameCalculator {
+	public void hesapla() {
+		System.out.println("Puaniniz : 90");
+	}
+}
+
---
+++ abstractDemo/src/abstractDemo/MySqlServerDatabaseManager.java
@@ -0,0 +1,11 @@
+package abstractDemo;
+
+public class MySqlServerDatabaseManager extends BaseDatabaseManager {
+
+	@Override
+	public void getData() {
+		System.out.println("Veri getirildi : MySQL");
+		
+	}
+
+}
